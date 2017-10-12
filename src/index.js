c71d280a686c77195fbc8998fb37966824875301
Mrunalini_Diwale <mdiwale@kenzan.com>
2017-10-12 13:10:47 -0400

    Modify gitignore, package.json and index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-var http = require('http');
-var dt = require('./dateTimeModule');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("Hello! The current date and time is: " + dt.myDateTime());
-    res.end();
-}).listen(8080);
+console.log('Hello! The current date and time is: ' + Date());
