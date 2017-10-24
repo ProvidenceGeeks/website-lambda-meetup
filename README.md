db63f76a62b58eba9a02431f151aa1bd29308fc0
mdiwale <32274486+mdiwale@users.noreply.github.com>
2017-10-24 17:34:24 -0400

    Integration with Meetup.com API (#9)
    * Setup lambda_meetup with basic build
    
    * Modify Readme
    
    * Added eslintrc and package-lock.json
    
    * Modify eslintrc
    
    * Modify eslintrc and package.json
    
    * Regenerate yarn.lock
    
    * Modify package.json to add request module
    
    * Modify index.js to integrate with meetup api
    
    * Modify index.js regarding syntax
    
    * integration with S3
    
    * linting
    
    * function name
    
    * organization
    
    * TODO
    
    * comments
    
    * linting
    
    * misc formatting
    
    * Modify index.js to flatten array
    
    * misc tweaks

--- README.md
+++ README.md
@@ -8,11 +8,18 @@ Repository for Lambda functionality related to periodically fetching data from t
 Please review the local development environment [steps](https://github.com/ProvidenceGeeks/website-docs/wiki/Onboarding-Guide#lambda) before getting started.
 
 1. Import the project in your IDE.
-2. In order to run the application, run `yarn run`
-
-
+2. In order to run the application (e.g. fetch Meetup data), run `yarn run fetch`
 
 ## Development
+To test locally, run
+```
+yarn run fetch
+```
+
+To test against S3, `export` _AWS_ACCESS_KEY_ID_ and _AWS_SECRET_ACCESS_KEY_ and run
+```
+export NODE_ENV=production && yarn run fetch
+```
 
 All available tasks are in the *scripts* section of *package.json*.
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,89 @@
-// eslint-disable-next-line no-console
-console.log('Hello! The current date and time is: ' + Date());
+/*
+ * This script assumes AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are exported as environment variables
+ */
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const request = require('request');
+const isProduction = process.env.NODE_ENV === 'production';
+const outputFile = 'meetup-data.json';
+const meetups = [
+  'Prov-JS',
+  'Kenzan-Providence-Hack-Nights',
+  'Girl-Develop-It-Providence',
+  'Tech-Collective-Meetups',
+  'MojoTech-Meetups',
+  'Rhode-Island-Makers-Microcontrollers-Robotics-Meetup',
+  'ux-meetup',
+  'IntraCityGeeks',
+  'WordPressRI'
+];
 
+(function init() {
+  const promises = meetups.map(meetup => getMeetupEventsData(`https://api.meetup.com/${meetup}/events`));
+  const resolveAllPromises = isProduction ? resolveMeetupEventsDataS3 : resolveMeetupEventsDataLocal;
+
+  Promise.all(promises)
+    .then(resolveAllPromises)
+    .catch(handleError);
+})();
+
+function resolveMeetupEventsDataLocal(results) {
+  const outputPath = `./output/${outputFile}`;
+
+  fs.writeFileSync(outputPath, formatResults(results));
+
+  console.log(`Successfully output data to ${outputPath}`); // eslint-disable-line
+}
+
+function resolveMeetupEventsDataS3(results) {
+  const s3 = new AWS.S3();
+  const bucket = 'providencegeeks.com';
+  const key = `external-services-data/meetup/${outputFile}`;
+
+  s3.createBucket({ Bucket: bucket }, function(err) {
+
+    if (err) {
+      handleError(err);
+    } else {
+      const params = {
+        Bucket: bucket,
+        Key: key,
+        Body: formatResults(results),
+        ACL: 'public-read'
+      };
+
+      s3.putObject(params, function(err) {
+        if (err) {
+          handleError(err);
+        } else {
+          console.log(`Successfully uploaded data to ${bucket}/${key}`); // eslint-disable-line
+        }
+      });
+    }
+  });
+}
+
+function getMeetupEventsData(url) {
+  return new Promise(function(resolve, reject) {
+
+    request({
+      method: 'GET',
+      uri: url
+    }, function (error, response, body) {
+      if (error) {
+        reject(error);
+      }
+      resolve(JSON.parse(body));
+    });
+  });
+}
+
+function formatResults(results) {
+  results = [].concat.apply([], results);
+
+  return JSON.stringify(results, null, 2);
+}
+
+function handleError(error) {
+  console.log(`ERROR: ${error}.  Should probably log this somewhere`); // eslint-disable-line
+}
