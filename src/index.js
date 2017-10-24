ca79cba821122a34af628a7b30cbd815f9121abd
Owen Buckley <obuckley@kenzan.com>
2017-10-23 22:59:56 -0400

    integration with S3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-var fs;
-var promises;
-var request;
-
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const request = require('request');
+const isProduction = process.env.NODE_ENV === 'production';
 const meetups = [
   'Prov-JS',
   'Kenzan-Providence-Hack-Nights',
@@ -14,18 +14,62 @@ const meetups = [
   'WordPressRI'
 ];
 
-request = require('request');
-fs = require('fs');
+// When a meetup has no events, it returns an empty array.
+// we should probably create a hash map, e.g.
+/* {
+ *   "Prov-JS": [{
+ *     // array of events
+ *     }, {
+ *
+ *   }],
+ *   "Kenzan-Providence-Hack-Nights": [{
+ *      //etc
+ *   }]
+ */
 
-function getData(url) {
+function formatResults(results) {
+  return JSON.stringify(results, null, 2)
+}
+
+function resolveMeetupEventsDataLocal(results) {
+  fs.writeFile('./output/meetup-data.json', formatResults(results));
+}
+
+function resolveMeetupEventsDataProduction(results) {
+  const s3 = new AWS.S3();
+  const bucket = 'providencegeeks.com';
+  const key = 'external-services-data/meetup/meetup-data.json';
+
+  s3.createBucket({Bucket: bucket}, function(err, data) {
+
+    if (err) {
+      console.log(err);
+    } else {
+      const params = {
+        Bucket: bucket,
+        Key: key,
+        Body: formatResults(results),
+        ACL: 'public-read'
+      };
+
+      s3.putObject(params, function(err, data) {
+        if (err) {
+          console.error(err)
+        } else {
+          console.log(`Successfully uploaded data to ${bucket}/${key}`);
+        }
+      });
+
+    }
+  });
+}
+
+function getMeetupEventsData(url) {
   return new Promise(function(resolve, reject) {
 
-    request(
-      { method: 'GET',
+    request({ method: 'GET',
         uri: url
-      }
-      , function (error, response, body) {
-
+      }, function (error, response, body) {
         if (error) {
           reject(error);
         }
@@ -37,11 +81,13 @@ function getData(url) {
 }
 
 function init() {
-  promises = meetups.map(meetup => getData(`https://api.meetup.com/${meetup}/events`));
+  const promises = meetups.map(meetup => getMeetupEventsData(`https://api.meetup.com/${meetup}/events`));
+  const promiseResolver = isProduction ? resolveMeetupEventsDataProduction : resolveMeetupEventsDataLocal;
 
   Promise.all(promises)
-    .then(results => {
-      fs.writeFile('meetup-data.json', JSON.stringify(results, null, 2));
+    .then(promiseResolver)
+    .catch((err) => {
+      console.error(`ERROR: ${err}.  Should probably log this somewhere?`);
     });
 }
 
